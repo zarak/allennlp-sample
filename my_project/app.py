b28f33474b2b1d9b352ab903b60deba1f026fcfc
Zarak <7254237+zarak@users.noreply.github.com>
2020-11-11 13:26:52 +0500

    Add streamlit app

---
+++ my_project/app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from predict import SentenceClassifierPredictor
+from model import SimpleClassifier
+from allennlp.common import JsonDict
+from allennlp.data import Instance
+from allennlp.predictors import Predictor
+from allennlp.models import Model
+from dataset_reader import ClassificationTsvReader
+
+
+
+def build_model(vocab):
+    print("Building the model")
+    vocab_size = vocab.get_vocab_size("tokens")
+    embedder = BasicTextFieldEmbedder(
+        {"tokens": Embedding(embedding_dim=10, num_embeddings=vocab_size)})
+    encoder = BagOfEmbeddingsEncoder(embedding_dim=10)
+    return SimpleClassifier(vocab, embedder, encoder)
+
+
+if __name__ == "__main__":
+    # vocab = Vocabulary.from_files("results/vocabulary")
+    # model = build_model(vocab)
+    # Model.from_archive("results/model.tar.gz")
+    model = Model.from_archive("../results/model.tar.gz")
+    dataset_reader = ClassificationTsvReader()
+
+    predictor = SentenceClassifierPredictor(model, dataset_reader)
+
+    # output = predictor.predict('A good movie!')
+    # print([(model.vocab.get_token_from_index(label_id, 'labels'), prob)
+           # for label_id, prob in enumerate(output['probs'])])
+    # output = predictor.predict('This was a monstrous waste of time.')
+    # print([(model.vocab.get_token_from_index(label_id, 'labels'), prob)
+           # for label_id, prob in enumerate(output['probs'])])
+
+    inp = st.text_area('Enter the text from a news article below')
+    output = predictor.predict(inp)
+    print("OUTPUT", output)
+    st.write(f"The probability that this article is 'hyperpartisan' is {output['probs'][0]:.2f}.")
---
+++ my_project/predict.py
@@ -0,0 +1,13 @@
+from allennlp.common import JsonDict
+from allennlp.data import Instance
+from allennlp.predictors import Predictor
+
+@Predictor.register("sentence_classifier")
+class SentenceClassifierPredictor(Predictor):
+    def predict(self, sentence: str) -> JsonDict:
+        # This method is implemented in the base class.
+        return self.predict_json({"sentence": sentence})
+
+    def _json_to_instance(self, json_dict: JsonDict) -> Instance:
+        sentence = json_dict["sentence"]
+        return self._dataset_reader.text_to_instance(sentence)
